20ef314a6fa02315d7e5fe6cfd871a0fd60baa27
svalgyx <154222657+svalgyx@users.noreply.github.com>
2024-03-12 22:30:18 +0300

    Update Matrix.cs

--- Implementings/Matrix.cs
+++ Implementings/Matrix.cs
@@ -11,7 +11,7 @@ namespace HW3_4.Implementings
             array = new T[rows, columns];
         }
 
-        protected override void CreateByUser() {
+        protected override void UserCreate() {
             for (int i = 0; i < Rows; i++)
             {
                 string row = Console.ReadLine();
@@ -23,7 +23,7 @@ namespace HW3_4.Implementings
             }
         }
 
-        protected override void CreateByRandom() {
+        protected override void RandomCreate() {
             Random rnd = new Random();
             for (int i = 0; i < Rows; i++)
             {
@@ -38,11 +38,11 @@ namespace HW3_4.Implementings
         {
             if (base.Init)
             {
-                CreateByUser();
+                UserCreate();
             }
             else
             {
-                CreateByRandom();
+                RandomCreate();
             }
         }
 
